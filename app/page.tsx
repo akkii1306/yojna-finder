8fc743649066b153e97c88892eaab8825c18c6e5
akkii1306 <akanksha.230101012@iiitbh.ac.in>
2025-12-06 12:59:47 +0530

    Initialize Next.js + TypeScript + Tailwind project

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-slate-900 text-white">
+      <h1 className="text-3xl font-bold">
+        Yojana Finder 2.0
+      </h1>
+    </main>
+  );
+}
+
